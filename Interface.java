2b4b443eb55cd7de3a0dc823b26342b6273f460f
augustoalvesper <93775760+augustoalvesper@users.noreply.github.com>
2021-11-20 13:02:09 -0300

    Add files via upload

---
+++ Interface.java
@@ -0,0 +1,15 @@
+package AuxilioBrasil;
+
+public interface Interface {
+	abstract public String getNome();
+	abstract public void setNome(String nome);
+	double getRendaPerCapita();
+	void setRenda(double renda);
+	void setQntPessoas(int qntPessoas);
+	void setNome1(String nome);
+	String getNome1();
+	void setNome11(String nome);
+	double getRendaPerCapita1();
+	void setRenda1(double renda);
+	void setQntPessoas1(int qntPessoas);
+}
