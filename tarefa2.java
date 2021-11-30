f4663f4fbf16ab5f2a55ab4d9b628a219648fa4e
augustoalvesper <93775760+augustoalvesper@users.noreply.github.com>
2021-11-30 17:10:27 -0300

    Spring

--- tarefa2.java
+++ tarefa2.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tarefa1")
-public class tarefa1 {
+@RequestMapping("/tarefa2")
+public class tarefa2 {
 
 	@GetMapping
-	public String tarefa1 () {
-		return "Persistencia";
+	public String tarefa2 () {
+		return "Disciplina";
 	}
 }
